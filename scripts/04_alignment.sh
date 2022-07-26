4dea662daa97be104eefcbe6f1ece1356fa39697
SJayYang <stanley.j.yang@gmail.com>
2022-07-26 23:38:23 +0000

    Added command line args

--- scripts/04_alignment.sh
+++ scripts/04_alignment.sh
@@ -1,10 +1,11 @@
+#!/bin/bash
 # @Author: Chenchen Zhu <czhu>
 # @Email:  czhu@me.com
 # @Last modified by:   czhu
 # @Last modified time: 2021-02-26
-ncpu=30
+ncpu=20
 PROG=sw/run_minimap2.sh
-folder="."
+folder=$1
 indir=$folder/fastq_trimmed/
 outdir=$folder/alignments/
 mkdir -p $outdir
